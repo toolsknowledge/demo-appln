61b97af407012ad0d27650755b371dd11cc138dd
vpro <samba.pendela@gmail.com>
2026-06-27 13:49:04 +0530

    code

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
     res.send("CI/CD Pipeline Working Successfully");
 });
 
+app.get("/wish",(req,res)=>{
+    res.json({"msg":"hook working successfully !!!"});
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
